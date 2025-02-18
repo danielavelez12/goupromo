3424418eba300bf7a9c0e0e33ea72dba01ef10d5
Daniela Velez <daniela@alza.app>
2025-02-18 00:02:18 -0500

    fixes and improvements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import * as Sentry from "@sentry/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppContainer from "./components/AppContainer";
 import Navbar from "./components/Navbar";
@@ -10,31 +11,48 @@ import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import theme from "./theme";
 
+Sentry.init({
+  dsn: "https://90e1e9018e569479e44b4811fcc80c54@o4508838399639552.ingest.us.sentry.io/4508838436732928",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: [
+    "localhost",
+    "https://goupromo-frontend.onrender.com",
+  ],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 const App: React.FC = () => {
   return (
-    <ChakraProvider value={theme}>
-      <BrowserRouter>
-        <CartProvider>
-          <AuthProvider>
-            <AppContainer>
-              <Navbar />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route
-                  path="/dashboard"
-                  element={
-                    <ProtectedRoute>
-                      <Dashboard />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route path="/checkout" element={<Checkout />} />
-              </Routes>
-            </AppContainer>
-          </AuthProvider>
-        </CartProvider>
-      </BrowserRouter>
-    </ChakraProvider>
+    <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>}>
+      <ChakraProvider value={theme}>
+        <BrowserRouter>
+          <CartProvider>
+            <AuthProvider>
+              <AppContainer>
+                <Navbar />
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route
+                    path="/dashboard"
+                    element={
+                      <ProtectedRoute>
+                        <Dashboard />
+                      </ProtectedRoute>
+                    }
+                  />
+                  <Route path="/checkout" element={<Checkout />} />
+                </Routes>
+              </AppContainer>
+            </AuthProvider>
+          </CartProvider>
+        </BrowserRouter>
+      </ChakraProvider>
+    </Sentry.ErrorBoundary>
   );
 };
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,18 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "daniela-w3",
+    project: "goupromo"
+  })],
+
   optimizeDeps: {
     include: ["@chakra-ui/react"],
   },
+
+  build: {
+    sourcemap: true
+  }
 });
